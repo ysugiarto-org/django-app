d3bf69c0eade81892b83f6febab2e6903a95a512
ysugiarto <yoh.sugiarto@gmail.com>
2023-12-24 08:54:38 +0700

    Adding model tests

--- todo/tests/test_models.py
+++ todo/tests/test_models.py
@@ -1,14 +1,18 @@
 from django.test import TestCase
 
+from todo.models import Todo
 from authentication.models import User
 
 class TestModels(TestCase):
     
-    def test_create_user(self):
+    def test_create_todo(self):
         user = User.objects.create_user(username='User', email='e@mail.com')
         user.set_password('123456')
         user.save()
         
-        self.assertEqual(str(user), 'e@mail.com')
+        todo = Todo(owner=user, title='Buy milk', description='Do it')
+        todo.save()
+        
+        self.assertEqual(str(todo), 'Buy milk')
         
         
